b2a3e9b667421d945ab5e72226aa72c5e63b0fd5
Anna Corsi <acorsi@cea.fr>
2018-03-05 21:14:56 +0300

    in order to merge

--- macro/raw/BmnDataToRoot.C
+++ macro/raw/BmnDataToRoot.C
@@ -22,7 +22,7 @@ void BmnDataToRoot(TString file, Long_t nEvents = 0, Bool_t doConvert = kTRUE)
     setup[1] = 0; // MWPC
     setup[2] = 0; // SILICON
     setup[3] = 0; // GEM
-    setup[4] = 0; // TOF-400
+    setup[4] = 1; // TOF-400
     setup[5] = 0; // TOF-700
     setup[6] = 0; // DCH
     setup[7] = 0; // ZDC
