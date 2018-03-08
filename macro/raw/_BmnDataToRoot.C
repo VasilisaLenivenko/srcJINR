0a1944e9088d0b6b9a5cdfe30e0bb2ccabf94a7d
hhhhh <hhh@hhh.de>
2018-03-09 01:03:20 +0300

    merge iwth my dev

--- macro/raw/_BmnDataToRoot.C
+++ macro/raw/_BmnDataToRoot.C
@@ -1,11 +1,11 @@
-#include "../run/bmnloadlibs.C"
-#include "../../bmndata/BmnEnums.h"
+#include "/nica/user/s/segarrae/software/bmnroot/macro/run/bmnloadlibs.C"
+#include "/nica/user/s/segarrae/software/bmnroot/bmndata/BmnEnums.h"
 
 //file: full path to raw-file
 //nEvents: if 0 then decode all events
 //doConvert: convert RAW --> ROOT before decoding or use file converted before
 
-void BmnDataToRoot(TString file, Long_t nEvents = 0, Bool_t doConvert = kTRUE)
+void BmnDataToRoot(TString file="/nica/mpd16/basalaev/src_raw_data2196-2311/mpd_run_trigCode_FILENAME.data", Long_t nEvents, Bool_t doConvert = kTRUE)
 {
 #if ROOT_VERSION_CODE < ROOT_VERSION(5,99,99)
     gROOT->LoadMacro("$VMCWORKDIR/macro/run/bmnloadlibs.C");
@@ -23,7 +23,7 @@ void BmnDataToRoot(TString file, Long_t nEvents = 0, Bool_t doConvert = kTRUE)
     setup[2] = 0; // SILICON
     setup[3] = 1; // GEM
     setup[4] = 1; // TOF-400
-    setup[5] = 1; // TOF-700
+    setup[5] = 0; // TOF-700
     setup[6] = 0; // DCH
     setup[7] = 0; // ZDC
     setup[8] = 0; // ECAL
@@ -32,7 +32,7 @@ void BmnDataToRoot(TString file, Long_t nEvents = 0, Bool_t doConvert = kTRUE)
 
     TString PeriodSetupExt = Form("%d%s.txt", period, ((stp == kBMNSETUP) ? "" : "_SRC"));
     decoder->SetTrigMapping(TString("Trig_map_Run") + PeriodSetupExt);
-    decoder->SetTrigINLFile("TRIG_INL_076D-16A8.txt");//INL only for TDC data 
+    decoder->SetTrigINLFile("TRIG_INL.txt");
     decoder->SetSiliconMapping("SILICON_map_run7.txt");
     decoder->SetGemMapping(TString("GEM_map_run") + PeriodSetupExt);
     // in case comment out the line decoder->SetTof400Mapping("...")  
@@ -41,14 +41,14 @@ void BmnDataToRoot(TString file, Long_t nEvents = 0, Bool_t doConvert = kTRUE)
     decoder->SetTof700Mapping("TOF700_map_period_7.txt");
     decoder->SetZDCMapping("ZDC_map_period_5.txt");
     decoder->SetZDCCalibration("zdc_muon_calibration.txt");
-    decoder->SetECALMapping("ECAL_map_period_7.txt");
+    decoder->SetECALMapping("ECAL_map_period_5.txt");
     decoder->SetECALCalibration("");
     decoder->SetMwpcMapping(TString("MWPC_map_period") + PeriodSetupExt);
     decoder->SetLANDMapping("land_mapping_jinr_triplex.txt");
     decoder->SetLANDPedestal("r0030_land_clock.hh");
-    decoder->SetLANDTCal("r0030jinr_land_tcal.hh");
+    decoder->SetLANDTCal("r0030_land_tcal.hh");
     decoder->SetLANDDiffSync("r352_cosmic1.hh");
-    decoder->SetLANDVScint("neuland_sync_0.txt");
+    decoder->SetLANDVScint("neuland_sync_2.txt");
     decoder->InitMaps();
     if (doConvert) decoder->ConvertRawToRoot(); // Convert raw data in .data format into adc-,tdc-, ..., sync-digits in .root format
     decoder->DecodeDataToDigi(); // Decode data into detector-digits using current mappings.
